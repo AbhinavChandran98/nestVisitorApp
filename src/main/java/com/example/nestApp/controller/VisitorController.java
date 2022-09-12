fbdedfd2fb04918d6871bcf39ea2d5ffb8400af2
AbhinavChandran98 <abhinavchandran0@gmail.com>
2022-09-12 14:34:02 +0530

    search added

--- src/main/java/com/example/nestApp/controller/VisitorController.java
+++ src/main/java/com/example/nestApp/controller/VisitorController.java
@@ -5,25 +5,28 @@ import com.example.nestApp.model.VisitorModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class VisitorController {
     @Autowired
     VisitorDao dao;
 
+//adding Employee
     @CrossOrigin(origins = "*")
     @PostMapping("/add")
     public String addEmployee(@RequestBody VisitorModel obj){
         dao.save(obj);
         return "employee added";
     }
-
+//delete employee
     @CrossOrigin(origins = "*")
-    @PostMapping("/delete")
+    @DeleteMapping("/delete/{id}")
     public String deleteEmployee(@PathVariable int id){
         dao.deleteById(id);
         return "employee deleted";
     }
-
+//update employee
     @CrossOrigin(origins = "*")
     @PostMapping("/update")
     public String updateEmployee(@RequestBody VisitorModel obj){
@@ -38,6 +41,27 @@ public class VisitorController {
         dao.updateEmployee(emp_code,mobile,name,password,username,desination,id);
         return "updated";
     }
+//search employee
+    @CrossOrigin(origins = "*")
+    @GetMapping("/search")
+    public List<VisitorModel> search(@RequestBody VisitorModel obj){
+        int id=obj.getId();
+        int emp_code=obj.getEmp_code();
+        int mobile=obj.getMobile();
+        String name= obj.getName();
+        String username=obj.getUsername();
+        String designation=obj.getDesignation();
+        return dao.search(name,id,emp_code,username,designation,mobile);
+    }
 
 
+    //login
+    @CrossOrigin(origins = "*")
+    @GetMapping("/login")
+    public String login(@RequestBody VisitorModel obj){
+        String username=obj.getUsername();
+        String password= obj.getPassword();
+        if(dao.l)
+
+    }
 }
--- src/main/java/com/example/nestApp/dao/VisitorDao.java
+++ src/main/java/com/example/nestApp/dao/VisitorDao.java
@@ -6,11 +6,21 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface VisitorDao extends CrudRepository<VisitorModel,Integer> {
 
+    //update query
     @Transactional
     @Modifying
     @Query(value = "UPDATE `employee` SET `emp_code`=?1,`mobile`=?2,`name`=?3,`password`=?4,`username`=?5,`designation`=?6 WHERE 'id'=?7",nativeQuery = true)
     void updateEmployee(int emp_code,int mobile,String name,String password,String username,String designation,int id);
+
+    //search query
+    @Query(value = "SELECT * FROM `employee` WHERE name=?1 || id=?2 || emp_code=?3 ||username=?4 || designation=?5 ||mobile=?6",nativeQuery = true)
+    List<VisitorModel> search(String name,int id,int emp_code,String username,String designation,int mobile);
+
+
+    //login querry
+    @Query(value = "SELECT `id`, `emp_code`, `name`, `mobile`,`designation` FROM `employee` WHERE `username`=?1 AND `password`=?2")
 }
