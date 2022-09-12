7985ac0e333ecdd96384f7499d5d8005e196fffc
AbhinavChandran98 <abhinavchandran0@gmail.com>
2022-09-12 11:14:17 +0530

    master added

---
+++ src/test/java/com/example/nestApp/NestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
